76dbee5905fac764c73ccd618dde93e7f84ffaec
Bruno B. de Souza <knuxbbs@gmail.com>
2020-04-23 14:31:42 -0300

    Remove Entity Framework dependencies

--- src/IdentityServerBuilderConfigurationExtensions.cs
+++ src/IdentityServerBuilderConfigurationExtensions.cs
@@ -5,14 +5,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using IdentityServer4.Configuration;
-using IdentityServer4.EntityFramework.Interfaces;
 using IdentityServer4.Hosting;
 using IdentityServer4.Models;
 using IdentityServer4.Stores;
 using IdentityServer4.Validation;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer.Configuration;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
@@ -29,14 +27,12 @@ namespace Microsoft.Extensions.DependencyInjection
         /// Configures defaults for Identity Server for ASP.NET Core scenarios.
         /// </summary>
         /// <typeparam name="TUser">The <typeparamref name="TUser"/> type.</typeparam>
-        /// <typeparam name="TContext">The <typeparamref name="TContext"/> type.</typeparam>
         /// <param name="builder">The <see cref="IIdentityServerBuilder"/>.</param>
         /// <returns>The <see cref="IIdentityServerBuilder"/>.</returns>
-        public static IIdentityServerBuilder AddApiAuthorization<TUser, TContext>(
+        public static IIdentityServerBuilder AddApiAuthorization<TUser>(
             this IIdentityServerBuilder builder) where TUser : class
-            where TContext : DbContext, IPersistedGrantDbContext
         {
-            builder.AddApiAuthorization<TUser, TContext>(o => { });
+            builder.AddApiAuthorization<TUser>(o => { });
             return builder;
         }
 
@@ -44,16 +40,13 @@ namespace Microsoft.Extensions.DependencyInjection
         /// Configures defaults on Identity Server for ASP.NET Core scenarios.
         /// </summary>
         /// <typeparam name="TUser">The <typeparamref name="TUser"/> type.</typeparam>
-        /// <typeparam name="TContext">The <typeparamref name="TContext"/> type.</typeparam>
         /// <param name="builder">The <see cref="IIdentityServerBuilder"/>.</param>
         /// <param name="configure">The <see cref="Action{ApplicationsOptions}"/>
         /// to configure the <see cref="ApiAuthorizationOptions"/>.</param>
         /// <returns>The <see cref="IIdentityServerBuilder"/>.</returns>
-        public static IIdentityServerBuilder AddApiAuthorization<TUser, TContext>(
+        public static IIdentityServerBuilder AddApiAuthorization<TUser>(
             this IIdentityServerBuilder builder,
-            Action<ApiAuthorizationOptions> configure)
-                where TUser : class
-                where TContext : DbContext, IPersistedGrantDbContext
+            Action<ApiAuthorizationOptions> configure) where TUser : class
         {
             if (configure == null)
             {
@@ -61,7 +54,7 @@ namespace Microsoft.Extensions.DependencyInjection
             }
 
             builder.AddAspNetIdentity<TUser>()
-                .AddOperationalStore<TContext>()
+                .AddHibernateOperationalStore()
                 .ConfigureReplacedServices()
                 .AddIdentityResources()
                 .AddApiResources()
@@ -98,7 +91,9 @@ namespace Microsoft.Extensions.DependencyInjection
                 ServiceDescriptor.Singleton<IConfigureOptions<ApiAuthorizationOptions>, ConfigureApiResources>(sp =>
                 {
                     var logger = sp.GetRequiredService<ILogger<ConfigureApiResources>>();
-                    var effectiveConfig = configuration ?? sp.GetRequiredService<IConfiguration>().GetSection("IdentityServer:Resources");
+                    var effectiveConfig = configuration ??
+                                          sp.GetRequiredService<IConfiguration>()
+                                              .GetSection("IdentityServer:Resources");
                     var localApiDescriptor = sp.GetService<IIdentityServerJwtDescriptor>();
                     return new ConfigureApiResources(effectiveConfig, localApiDescriptor, logger);
                 }));
@@ -136,12 +131,14 @@ namespace Microsoft.Extensions.DependencyInjection
             builder.ConfigureReplacedServices();
             builder.AddInMemoryIdentityResources(Enumerable.Empty<IdentityResource>());
             builder.Services.TryAddEnumerable(
-                ServiceDescriptor.Singleton<IConfigureOptions<ApiAuthorizationOptions>, ConfigureIdentityResources>(sp =>
-                {
-                    var logger = sp.GetRequiredService<ILogger<ConfigureIdentityResources>>();
-                    var effectiveConfig = configuration ?? sp.GetRequiredService<IConfiguration>().GetSection("IdentityServer:Identity");
-                    return new ConfigureIdentityResources(effectiveConfig, logger);
-                }));
+                ServiceDescriptor.Singleton<IConfigureOptions<ApiAuthorizationOptions>, ConfigureIdentityResources>(
+                    sp =>
+                    {
+                        var logger = sp.GetRequiredService<ILogger<ConfigureIdentityResources>>();
+                        var effectiveConfig = configuration ?? sp.GetRequiredService<IConfiguration>()
+                            .GetSection("IdentityServer:Identity");
+                        return new ConfigureIdentityResources(effectiveConfig, logger);
+                    }));
 
             // We take over the setup for the identity resources as Identity Server registers the enumerable as a singleton
             // and that prevents normal composition.
@@ -181,11 +178,12 @@ namespace Microsoft.Extensions.DependencyInjection
 
             builder.Services.TryAddEnumerable(
                 ServiceDescriptor.Singleton<IConfigureOptions<ApiAuthorizationOptions>, ConfigureClients>(sp =>
-                 {
-                     var logger = sp.GetRequiredService<ILogger<ConfigureClients>>();
-                     var effectiveConfig = configuration ?? sp.GetRequiredService<IConfiguration>().GetSection("IdentityServer:Clients");
-                     return new ConfigureClients(effectiveConfig, logger);
-                 }));
+                {
+                    var logger = sp.GetRequiredService<ILogger<ConfigureClients>>();
+                    var effectiveConfig = configuration ??
+                                          sp.GetRequiredService<IConfiguration>().GetSection("IdentityServer:Clients");
+                    return new ConfigureClients(effectiveConfig, logger);
+                }));
 
             // We take over the setup for the clients as Identity Server registers the enumerable as a singleton and that prevents normal composition.
             builder.Services.AddSingleton<IEnumerable<Client>>(sp =>
@@ -218,12 +216,14 @@ namespace Microsoft.Extensions.DependencyInjection
         {
             builder.ConfigureReplacedServices();
             builder.Services.TryAddEnumerable(
-                ServiceDescriptor.Singleton<IConfigureOptions<ApiAuthorizationOptions>, ConfigureSigningCredentials>(sp =>
-                {
-                    var logger = sp.GetRequiredService<ILogger<ConfigureSigningCredentials>>();
-                    var effectiveConfig = configuration ?? sp.GetRequiredService<IConfiguration>().GetSection("IdentityServer:Key");
-                    return new ConfigureSigningCredentials(effectiveConfig, logger);
-                }));
+                ServiceDescriptor.Singleton<IConfigureOptions<ApiAuthorizationOptions>, ConfigureSigningCredentials>(
+                    sp =>
+                    {
+                        var logger = sp.GetRequiredService<ILogger<ConfigureSigningCredentials>>();
+                        var effectiveConfig = configuration ??
+                                              sp.GetRequiredService<IConfiguration>().GetSection("IdentityServer:Key");
+                        return new ConfigureSigningCredentials(effectiveConfig, logger);
+                    }));
 
             // We take over the setup for the credentials store as Identity Server registers a singleton
             builder.Services.AddSingleton<ISigningCredentialStore>(sp =>
@@ -251,7 +251,8 @@ namespace Microsoft.Extensions.DependencyInjection
 
         internal static IIdentityServerBuilder ConfigureReplacedServices(this IIdentityServerBuilder builder)
         {
-            builder.Services.TryAddEnumerable(ServiceDescriptor.Transient<IConfigureOptions<IdentityServerOptions>, AspNetConventionsConfigureOptions>());
+            builder.Services.TryAddEnumerable(ServiceDescriptor
+                .Transient<IConfigureOptions<IdentityServerOptions>, AspNetConventionsConfigureOptions>());
             builder.Services.TryAddSingleton<IAbsoluteUrlFactory, AbsoluteUrlFactory>();
             builder.Services.AddSingleton<IRedirectUriValidator, RelativeRedirectUriValidator>();
             builder.Services.AddSingleton<IClientRequestParametersProvider, DefaultClientRequestParametersProvider>();
@@ -266,9 +267,9 @@ namespace Microsoft.Extensions.DependencyInjection
             // on the container and replacing the instance. This is due to the fact that we chain on AddIdentityServer which configures the
             // list of endpoints by default.
             var endSessionEndpointDescriptor = builder.Services
-                            .Single(s => s.ImplementationInstance is Endpoint e &&
-                                    string.Equals(e.Name, "Endsession", StringComparison.OrdinalIgnoreCase) &&
-                                    string.Equals("/connect/endsession", e.Path, StringComparison.OrdinalIgnoreCase));
+                .Single(s => s.ImplementationInstance is Endpoint e &&
+                             string.Equals(e.Name, "Endsession", StringComparison.OrdinalIgnoreCase) &&
+                             string.Equals("/connect/endsession", e.Path, StringComparison.OrdinalIgnoreCase));
 
             builder.Services.Remove(endSessionEndpointDescriptor);
             builder.AddEndpoint<AutoRedirectEndSessionEndpoint>("EndSession", "/connect/endsession");
